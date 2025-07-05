4800140ff92a73231f9d6336aa9dc1099541a184
Taha Toklucu <tahatoklucu@hotmail.com>
2025-07-05 13:40:23 +0200

    Update
    Update

--- weather-app/src/App.jsx
+++ weather-app/src/App.jsx
@@ -1,9 +1,14 @@
 import './App.css'
+import {Route, Routes} from 'react-router-dom';
+import Home from './components/Home';
 
 function App() {
 
   return (
     <>
+      <Routes>
+        <Route path='/' exact element={<Home />}  />
+      </Routes>
     </>
   )
 }
--- weather-app/src/components/Home.jsx
+++ weather-app/src/components/Home.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
-import {Route, Routes} from 'react-router-dom';
-import '../styles/Main.css';
+import '../styles/Home.css';
 
 function Main() {
   return (
