ef57297dc0130ef288f9f538f6a79efc7364a165
Taha Toklucu <tahatoklucu@hotmail.com>
2025-07-05 13:25:34 +0200

    Initial commit

---
+++ weather-app/src/components/Main.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import {Route, Routes} from 'react-router-dom';
+import '../styles/Main.css';
+
+function Main() {
+  return (
+    <div className='container'>
+        <div className='top-section'>
+            <h4 className='top-title'>Weather App</h4>
+        </div>
+    </div>
+  )
+}
+
+export default Main
