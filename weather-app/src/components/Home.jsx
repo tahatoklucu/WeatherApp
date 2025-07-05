a8b3eef259e3a06cc2fcd835eab4e6e1b5ee59ce
Taha Toklucu <tahatoklucu@hotmail.com>
2025-07-05 14:09:41 +0200

    Update
    Update

--- weather-app/src/components/Home.jsx
+++ weather-app/src/components/Home.jsx
@@ -3,11 +3,13 @@ import '../styles/Home.css';
 
 function Main() {
   return (
-    <div className='container bg-sky-50 size-500'>
+    <>
+    <div className='container'>
         <div className='top-section'>
             <h4 className='top-title'>Weather App</h4>
         </div>
     </div>
+    </>
   )
 }
 
