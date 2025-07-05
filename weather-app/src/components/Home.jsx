01d7b05122610980ebe74e2cfb9e6c30207cbe26
Taha Toklucu <tahatoklucu@hotmail.com>
2025-07-05 14:18:47 +0200

    Update
    Update

--- weather-app/src/components/Home.jsx
+++ weather-app/src/components/Home.jsx
@@ -4,8 +4,8 @@ import '../styles/Home.css';
 function Main() {
   return (
     <>
-    <div className='container'>
-        <div className='top-section'>
+    <div className='container text-center flex justify-center'>
+        <div className='top-section rounded-sm border-1 inset-shadow-sm w-100'>
             <h4 className='top-title'>Weather App</h4>
         </div>
     </div>
