825f5cc329b62776f5c9ebb4713c5f88d64793ce
Taha Toklucu <tahatoklucu@hotmail.com>
2025-07-06 14:43:31 +0200

    Update
    Update

--- weather-app/src/components/Home.jsx
+++ weather-app/src/components/Home.jsx
@@ -4,9 +4,10 @@ function Main() {
   return (
     <>
     <div className='container p-5 w-full justify-center mx-auto'>
-        <div className='top-section text-center justify-center rounded-md border-1 border-[#dee0ea] inset-shadow-sm w-full p-8'>
-            <h4 className='top-title text-[#dee0ea]'>Weather App</h4>
+        <div className='top-section text-center justify-center rounded-md border-1 border-[#749BC2] inset-shadow-sm w-full p-8 shadow-md bg-transparent'>
+            <h4 className='top-title text-[#dee0ea] text-2xl text-shadow-md'>Weather App</h4>
         </div>
+        <div className='left-side'></div>
     </div>
     </>
   )
