90ec7c759842401607223afdd3d09817d1c42d16
Taha Toklucu <tahatoklucu@hotmail.com>
2025-07-06 15:08:10 +0200

    Update
    Update

--- weather-app/src/components/Home.jsx
+++ weather-app/src/components/Home.jsx
@@ -7,7 +7,7 @@ function Main() {
     <>
     <div className='container p-5 w-full justify-center mx-auto'>
         <div className='text-center justify-center flex rounded-lg border-1 border-[#749BC2] inset-shadow-sm w-full p-5 shadow-md'>
-            <label className='text-left justify-left p-2'>Weather App</label>
+            <label className='text-left justify-left p-1'>Weather App</label>
             <SearchIcon />
             <input type='text' placeholder='Select country or city' className='w-200 mx-auto bg-[#dee0ea] p-2 rounded-md shadow-lg' />
             <SettingsIcon />
