44c421d1aaeaaf8c468bf559be1eb791e032f2a1
Taha Toklucu <tahatoklucu@hotmail.com>
2025-07-06 15:06:07 +0200

    Update
    Update

--- weather-app/src/components/Home.jsx
+++ weather-app/src/components/Home.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
+import SearchIcon from '@mui/icons-material/Search';
+import SettingsIcon from '@mui/icons-material/Settings';
 
 function Main() {
   return (
     <>
     <div className='container p-5 w-full justify-center mx-auto'>
-        <div className='top-section text-center justify-center rounded-md border-1 border-[#749BC2] inset-shadow-sm w-full p-8 shadow-md bg-transparent'>
-            <h4 className='top-title text-[#dee0ea] text-2xl text-shadow-md'>Weather App</h4>
+        <div className='text-center justify-center flex rounded-lg border-1 border-[#749BC2] inset-shadow-sm w-full p-5 shadow-md'>
+            <label className='text-left justify-left p-2'>Weather App</label>
+            <SearchIcon />
+            <input type='text' placeholder='Select country or city' className='w-200 mx-auto bg-[#dee0ea] p-2 rounded-md shadow-lg' />
+            <SettingsIcon />
         </div>
         <div className='left-side'></div>
     </div>
