7b73fcde855f7f861d778f6d1fb1869b1839dc98
Taha Toklucu <tahatoklucu@hotmail.com>
2025-07-09 15:55:13 +0200

    Update
    Update

--- weather-app/src/components/Home.jsx
+++ weather-app/src/components/Home.jsx
@@ -63,8 +63,8 @@ function Main() {
     <>
     <div className='container p-5 w-full justify-center mx-auto'>
         <div className='text-center justify-center flex rounded-lg border-1 border-[#749BC2] inset-shadow-sm w-full p-5 shadow-md'>
-            <label className='text-left justify-left m-2 text-[#dee0ea] text-xl h-auto md:h-5vh'>Weather App</label>
-            <input type='text' onChange={(e) => setLocation(e.target.value)} placeholder='Select country or city' className='w-310 mx-auto bg-[#dee0ea] p-2 rounded-md shadow-lg' />
+            <label className='text-left justify-left m-2 text-[#dee0ea] text-xl h-auto md:h-5vh drop-shadow-lg'>Weather App</label>
+            <input type='text' onChange={(e) => setLocation(e.target.value)} placeholder='Enter a city' className='w-310 mx-auto bg-[#dee0ea] p-2 rounded-md shadow-lg' />
             <SettingsIcon className='text-white m-2 cursor-pointer mr-5 md:h-5px' />
         </div>
         <div>
@@ -72,14 +72,14 @@ function Main() {
               <div className='p-10 border-1 mt-10 border-[#749BC2] rounded-lg inset-shadow-sm shadow-md'>
                   <div className='flex relative'>
                     <img src={weatherImage} className='w-80 h-80 text-center' />
-                    <div className='border-l-1 border-[#dee0ea] ml-10'>
-                      <h4 className='text-white text-shadow-lg text-2xl ml-10 uppercase'>5 day weather forecast:</h4>
+                    <div className='border-l-1 border-[#dee0ea] ml-10 drop-shadow-xs'>
+                      <h4 className='text-white text-shadow-lg text-2xl ml-10 uppercase drop-shadow'>5 day weather forecast:</h4>
                     </div>
                   </div>
                   <div className='p-2 text-2xl'>
-                    <h2 className='text-white text-shadow-lg text-6xl mb-5 mt-5 '>{weather.location.name}, {weather.location.country}</h2>
-                    <p className='text-white text-shadow-lg text-3xl'>Temperature: {weather.current.temp_c}°C</p>
-                    <p className='text-white text-shadow-lg text-3xl mt-4'>Condition: {weather.current.condition.text}</p>
+                    <h2 className='text-white text-shadow-lg text-6xl mb-5 mt-5 drop-shadow-lg'>{weather.location.name}, {weather.location.country}</h2>
+                    <p className='text-white text-shadow-lg text-3xl drop-shadow-lg'>Temperature: {weather.current.temp_c}°C</p>
+                    <p className='text-white text-shadow-lg text-3xl mt-4 drop-shadow-lg'>Condition: {weather.current.condition.text}</p>
                   </div>
               </div>
             ):
