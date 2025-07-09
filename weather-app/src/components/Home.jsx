ff28e9e70316faa49ba39eb803da01de815844f1
Taha Toklucu <tahatoklucu@hotmail.com>
2025-07-09 02:19:14 +0200

    Update
    Update

--- weather-app/src/components/Home.jsx
+++ weather-app/src/components/Home.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import SettingsIcon from '@mui/icons-material/Settings';
 import axios from 'axios';
+import Rainy from '../assets/Rainy.png';
 
 function Main() {
     const [weather, setWeather] = useState([]);
@@ -25,15 +26,18 @@ function Main() {
     <div className='container p-5 w-full justify-center mx-auto'>
         <div className='text-center justify-center flex rounded-lg border-1 border-[#749BC2] inset-shadow-sm w-full p-5 shadow-md'>
             <label className='text-left justify-left p-2 text-[#dee0ea] text-xl h-auto md:h-5vh'>Weather App</label>
-            <input type='text' onSubmit={(e) => setLocation(e.target.value)} placeholder='Select country or city' className='w-310 mx-auto bg-[#dee0ea] p-2 rounded-md shadow-lg' />
+            <input type='text' onChange={(e) => setLocation(e.target.value)} placeholder='Select country or city' className='w-310 mx-auto bg-[#dee0ea] p-2 rounded-md shadow-lg' />
             <SettingsIcon className='text-white m-2 cursor-pointer mr-5 md:h-5px' />
         </div>
         <div className='left-side'>
             {weather && weather.location ? (
-              <div>
-                  <h2>{weather.location.name}, {weather.location.country}</h2>
-                  <p>Temperature: {weather.current.temp_c}°C</p>
-                  <p>Condition: {weather.current.condition.text}</p>
+              <div className='p-10 border-1 mt-10 border-[#749BC2] rounded-lg inset-shadow-sm shadow-md text-center justify-center'>
+                  <img src={Rainy} className='w-70 h-70 text-center'></img>
+                  <div className='p-2 text-2xl'>
+                    <h2 className='text-white'>{weather.location.name}, {weather.location.country}</h2>
+                    <p className='text-white'>Temperature: {weather.current.temp_c}°C</p>
+                    <p className='text-white'>Condition: {weather.current.condition.text}</p>
+                  </div>
               </div>
             ):
               <p>Veri yükleniyor veya bulunamadı.</p>
